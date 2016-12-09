56ed44f6f0065b1972b1167294fbc28746e9f3aa
Ingo Fischer <github@fischer-ka.de>
2016-12-09 16:37:11 +0100

    try setting announce mp3
    … to get rid of the failing

--- test/testAdapter.js
+++ test/testAdapter.js
@@ -84,7 +84,7 @@ describe('Test ' + adapterShortName + ' adapter', function() {
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
 
-            //config.native.dbtype   = 'sqlite';
+            config.native.announce   = 'gong.mp3';
 
             setup.setAdapterConfig(config.common, config.native);
 
